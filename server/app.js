122b701c6fff6174ca0c02ca5932c467a73f6117
bejeju <bejejupark@gmail.com>
2022-03-23 00:12:12 +0900

    21.03.23

--- server/app.js
+++ server/app.js
@@ -1,14 +1,5 @@
 const express = require('express');
-const { sequelize } = require('./models')
 const app = express();
-
-sequelize.sync({ force: false })
-.then(()=>{
-    console.log('데이터베이스 연결성공')
-})
-.catch((err)=>{
-    console.error(err);
-})
 
 app.listen(8080, ()=>{
     console.log('8080포트 서버가 실행되었습니다.');
